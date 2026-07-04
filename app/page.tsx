e38596e3cecc0da97f43ada20ed0d1fb5f82eb86
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2026-07-04 16:59:26 -0600

    feat(component): se agrego el componente Hero

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import ContactCTA from "@/src/components/ContactCTA/ContactCTA";
 import Portfolio from "@/src/components/Portfolio/Portfolio";
 import Process from "@/src/components/Process/Process";
 import Services from "@/src/components/Services/Services";
+import Hero from "@/src/Hero/Hero";
 
 export default function Home() {
   return (
     <main>
+      <Hero />
       <Portfolio />
       <ContactCTA />
       <Services />
